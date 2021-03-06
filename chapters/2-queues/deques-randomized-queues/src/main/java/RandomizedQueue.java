6eab28dbc45025ea38450d4401462c518f6bc73d
Joseph Cooper <cooper.joseph@gmail.com>
2021-03-06 15:20:18 +0000

    Add implementation of RandomizedQueue

---
+++ chapters/2-queues/deques-randomized-queues/src/main/java/RandomizedQueue.java
@@ -0,0 +1,90 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomizedQueue<E> implements Iterable<E> {
+
+  public static final int ARRAY_INITIAL_SIZE = 2;
+  private Object[] items = new Object[ARRAY_INITIAL_SIZE];
+  private int size = 0;
+
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public void enqueue(final E item) {
+    if (item == null) throw new IllegalArgumentException();
+
+    if (size() >= items.length) { // Need to resize the array
+      final int newArraySize;
+      Object[] newItems = new Object[items.length * 2];
+      System.arraycopy(items, 0, newItems, 0, items.length);
+      items = newItems;
+    }
+    items[size()] = item;
+    size += 1;
+  }
+
+  public E dequeue() {
+    if (size() < 1) throw new NoSuchElementException();
+
+    final int indexToRemove = StdRandom.uniform(size);
+
+    @SuppressWarnings( "unchecked" )
+    E value = (E) items[indexToRemove];
+
+    final int indexOfLastItem = size() - 1;
+    items[indexToRemove] = items[indexOfLastItem];
+    items[indexOfLastItem] = null;
+    size -= 1;
+
+    if (size() < items.length / 4) { // Resize array
+      Object[] newItems = new Object[items.length / 2];
+      System.arraycopy(items, 0, newItems, 0, size());
+      items = newItems;
+    }
+    return value;
+  }
+
+  @SuppressWarnings( "unchecked" )
+  public E sample() {
+    if (size() < 1) throw new NoSuchElementException();
+    return (E) items[StdRandom.uniform(size())];
+  }
+
+  @Override public Iterator<E> iterator() {
+    return new RandomizedQueueIterator<>(items, size());
+  }
+
+  private static class RandomizedQueueIterator<E> implements Iterator<E> {
+
+    private final Object[] items;
+    int currentIndex = 0;
+
+    RandomizedQueueIterator(final Object[] itemsToIterate, final int count) {
+      items = new Object[count];
+      System.arraycopy(itemsToIterate, 0, items, 0, count);
+      StdRandom.shuffle(items);
+    }
+
+    @Override public boolean hasNext() {
+      return currentIndex < items.length - 1;
+    }
+
+    @SuppressWarnings( "unchecked" )
+    @Override
+    public E next() {
+      if (!hasNext()) throw new NoSuchElementException();
+      return (E) items[currentIndex++];
+    }
+
+    @Override public void remove() {
+      throw new UnsupportedOperationException();
+    }
+  }
+}
