3455b22e0410f5079dbc87cf580bf9e2348b4847
Joseph Cooper <cooper.joseph@gmail.com>
2021-03-17 14:42:23 +0000

    Cleanup

--- chapters/build.gradle.kts
+++ chapters/build.gradle.kts
@@ -16,8 +16,8 @@ subprojects {
   }
   dependencies {
     implementation("edu.princeton.cs:algs4:1.0.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testImplementation(project(":common"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
   }
   tasks.test {
