404cd18f0018b84f977feeaf202cbf64fc7c1f2f
Joseph Cooper <cooper.joseph@gmail.com>
2021-03-17 14:42:34 +0000

    Implement Manhattan distance function for Board

--- chapters/4-priority-queue/8puzzle/src/main/java/Board.java
+++ chapters/4-priority-queue/8puzzle/src/main/java/Board.java
@@ -1,30 +1,49 @@
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public final class Board {
 
-  private static final int SPACE = 0;
-  private final int[][] tiles;
+  private final Tile SPACE;
+  private final Tile[][] tiles;
+  private final int dimension;
 
   public Board(final int[][] tiles) {
     assert tiles != null;
-    assert 2 <= tiles.length && tiles.length < 128;
-    for (var row : tiles) assert row.length == tiles.length;
-    this.tiles = tiles;
+    dimension = tiles.length;
+    assert 2 <= dimension && dimension < 128;
+    for (var row : tiles) assert row.length == dimension;
+
+    this.SPACE = new Tile(dimension * dimension - 1, "0");
+
+    this.tiles = new Tile[dimension][dimension];
+
+    for (int row = 0; row < dimension; row++) {
+      for (int col = 0; col < dimension; col++) {
+        this.tiles[row][col] = new Tile(
+            labelToId(tiles[row][col]),
+            Integer.toString(tiles[row][col]));
+      }
+    }
+  }
+
+  private int labelToId(int label) {
+    if (label == 0) return SPACE.id;
+    return label - 1;
   }
 
   @Override public String toString() {
-    var joiner = new StringJoiner("\n").add(Integer.toString(dimension()));
-    for (final int[] row : tiles) {
+    var joiner = new StringJoiner("\n ").add(Integer.toString(dimension()));
+    for (final Tile[] row : tiles) {
       joiner.add(rowString(row));
     }
     return joiner.toString();
   }
 
-  private String rowString(int[] row) {
+  private String rowString(Tile[] row) {
     return Arrays.stream(row)
-        .mapToObj(Integer::toString)
+        .map(tile -> tile.label)
         .collect(Collectors.joining(" "));
   }
 
@@ -37,7 +56,31 @@ public final class Board {
   }
 
   public int manhattan() {
-    return Integer.MIN_VALUE;
+    var sum = 0;
+    for (int i = 0; i < dimension(); i++) {
+      for (int j = 0; j < dimension(); j++) {
+        final Tile tile = tiles[i][j];
+        if (!tile.equals(SPACE))
+          sum += tileManhattan(i, j);
+      }
+    }
+    return sum;
+  }
+
+  private int tileManhattan(int row, int col) {
+    final int entry = tiles[row][col].id;
+    return Math.abs(row - goalRowOfEntry(entry, dimension())) +
+        Math.abs(col - goalColOfEntry(entry, dimension()));
+  }
+
+  private static int goalRowOfEntry(final int entry, final int dimension) {
+    check(0 <= entry && entry < (dimension * dimension));
+    return entry / dimension;
+  }
+
+  private static int goalColOfEntry(int entry, int dimension) {
+    check(0 <= entry && entry < (dimension * dimension));
+    return entry % dimension;
   }
 
   public boolean isGoal() {
@@ -63,15 +106,40 @@ public final class Board {
 
   }
 
-  private static Board goalBoard(int size) {
-    assert size >= 2;
-    var tiles = new int[size][size];
-    for (int row = 0; row < size; row++) {
-      for (int col = 0; col < size; col++) {
-        tiles[row][col] = row * size + col + 1;
-      }
-    }
-    tiles[size - 1][size - 1] = Board.SPACE;
-    return new Board(tiles);
+  private static void check(boolean condition) {
+    check(condition, "");
   }
+
+  private static void check(boolean condition, String message) {
+    if (!condition) throw new IllegalArgumentException(message);
+  }
+
+  private static final class Tile {
+    public final int id;
+    public final String label;
+
+    private Tile(final int id, final String label) {
+      this.id = id;
+      this.label = label;
+    }
+
+    @Override public String toString() {
+      return "Tile{" +
+          "id=" + id +
+          ", label='" + label + '\'' +
+          '}';
+    }
+
+    @Override public boolean equals(final Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      final Tile tile = (Tile) o;
+      return id == tile.id && label.equals(tile.label);
+    }
+
+    @Override public int hashCode() {
+      return Objects.hash(id, label);
+    }
+  }
+
 }
--- chapters/4-priority-queue/8puzzle/src/test/java/BoardTest.java
+++ chapters/4-priority-queue/8puzzle/src/test/java/BoardTest.java
@@ -4,14 +4,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoardTest {
 
+  static int[] row(int... values) {
+    return values;
+  }
+
+  static Board board(int[]... rows) {
+    return new Board(rows);
+  }
+
   @Test
   void toString_returnsOutput() {
-    var board = new Board(new int[][]{
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 0}
-    });
-    var expected = "3\n1 2 3\n4 5 6\n7 8 0";
+    var board = board(
+        row(1, 2, 3),
+        row(4, 5, 6),
+        row(7, 8, 0)
+    );
+    var expected = "3\n 1 2 3\n 4 5 6\n 7 8 0";
     assertEquals(expected, board.toString());
   }
+
+  /*
+    The following board should have manhattan() == 1
+    1  2
+    0  3
+  */
+  @Test
+  void manhattan_returnsCorrectValueFor2x2() {
+    var board = board(
+        row(1, 2),
+        row(0, 3)
+    );
+    assertEquals(1, board.manhattan());
+  }
+
+  /*
+    The following board should have manhattan() == 10
+    8 1 3
+    4 0 2
+    7 6 5
+  */
+  @Test
+  void manhattan_returnsCorrectValue3x3() {
+    var board = board(
+        row(8, 1, 3),
+        row(4, 0, 2),
+        row(7, 6, 5)
+    );
+    assertEquals(10, board.manhattan());
+  }
+
 }
