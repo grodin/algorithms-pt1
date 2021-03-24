b1ce98d39294c3785c7efb25132828613c31503c
Joseph Cooper <cooper.joseph@gmail.com>
2021-03-24 13:03:34 +0000

    Implement PointSET class, delegating to ju.TreeSet

---
+++ chapters/5-balanced-binary-search-tree/kd-tree/src/main/java/PointSET.java
@@ -0,0 +1,51 @@
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+
+  private final Set<Point2D> points = new TreeSet<>();
+
+  public boolean isEmpty() {
+    return points.isEmpty();
+  }
+
+  public int size() {
+    return points.size();
+  }
+
+  public void insert(Point2D p) {
+    points.add(Objects.requireNonNull(p));
+  }
+
+  public boolean contains(Point2D p) {
+    return points.contains(Objects.requireNonNull(p));
+  }
+
+  public void draw() {
+    for (var point: points){
+      point.draw();
+    }
+  }
+
+  public Iterable<Point2D> range(RectHV rect) {
+    Objects.requireNonNull(rect);
+    return points.stream()
+        .filter(rect::contains)
+        .collect(Collectors.toUnmodifiableList());
+  }
+
+  public Point2D nearest(Point2D p) {
+    Objects.requireNonNull(p);
+    return points.stream()
+        .min(Comparator.comparing(p::distanceTo))
+        .orElse(null);
+  }
+
+  public static void main(String[] args) {}
+}
